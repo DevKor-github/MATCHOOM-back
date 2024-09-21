80eb8b24055b94014b079a580a5c4332f74072ec
liverforpresent <dailynsteady@gmail.com>
2024-09-21 23:21:23 +0900

    chore: es-hangul 라이브러리 추가

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // validation pope
+  // validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
