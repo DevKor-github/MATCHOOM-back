db99736d0823ef63ccdf1b82ad584c5234cd9b09
liverforpresent <dailynsteady@gmail.com>
2024-11-07 21:32:00 +0900

    feat: s3 초기 설정

--- src/s3/s3.module.ts
+++ src/s3/s3.module.ts
@@ -1,4 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { S3Client } from '@aws-sdk/client-s3';
+import { S3Service } from './s3.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lecture } from 'src/entities/lecture.entity';
+import { User } from 'src/entities/user.entity';
 
-@Module({})
+@Global()
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Lecture])],
+  providers: [
+    {
+      provide: 'S3_CLIENT',
+      useFactory: () => {
+        return new S3Client({
+          region: process.env.AWS_REGION,
+          credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          },
+        });
+      },
+    },
+    S3Service
+  ],
+  exports: ['S3_CLIENT']
+})
 export class S3Module {}
---
+++ src/s3/s3.service.ts
@@ -0,0 +1,17 @@
+import { S3Client } from '@aws-sdk/client-s3';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class S3Service {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @Inject('S3_CLIENT')
+    private readonly s3Client: S3Client
+  ) { }
+
+  
+}
