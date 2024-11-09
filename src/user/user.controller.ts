38b2be89a32f403fcc71f2e79330ce5d45b82f4c
liverforpresent <dailynsteady@gmail.com>
2024-11-09 22:09:28 +0900

    feat: file type validation pipe 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/decorator/user.decorator';
 import { UpdateUserDto } from './dtos/updateUser.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { FileValidationPipe } from 'src/common/pipes/fileValidation.pipe';
 
 @Controller('user')
 export class UserController {
@@ -14,7 +15,10 @@ export class UserController {
   @Patch('/')
   @UseGuards(AuthGuard('jwt-access'))
   @UseInterceptors(FileInterceptor('file', { limits: { fileSize: 5 * 1024 * 1024 } }))
-  async updateUser(@User() user, @Body() updateUserDto: UpdateUserDto, @UploadedFile() file: Express.Multer.File) {
+  async updateUser(
+    @User() user, @Body() updateUserDto: UpdateUserDto,
+    @UploadedFile(new FileValidationPipe(['image/jpg', 'image/jpeg', 'image/png'])) file: Express.Multer.File
+  ) {
     await this.userService.updateUser(user.id, updateUserDto, file);
   }
 }
