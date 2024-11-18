3760c9310c13b3a455e8c75fc5fdcdd21b4b1938
liverforpresent <dailynsteady@gmail.com>
2024-11-18 19:12:26 +0900

    fix: allowedHeaders 수정

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: 'Content-Type, Authorization, application/json',
   });
 
   // validation pipe
