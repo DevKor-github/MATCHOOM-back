06129665f2acee322dec021567d94bc4e137f23f
Na1gae <nalgae@korea.ac.kr>
2024-11-18 15:04:59 +0900

    Feat: Added elapsed time to logger

--- src/common/interceptors/logger.interceptor.ts
+++ src/common/interceptors/logger.interceptor.ts
@@ -5,13 +5,16 @@ import { tap, catchError } from 'rxjs/operators';
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const income = Date.now()
     const request = context.switchToHttp().getRequest();
     const response = context.switchToHttp().getResponse();
 
+    const elapse_time = Date.now()-income
     console.log('Request:', {
       method: request.method,
       url: request.url,
       headers: request.headers,
+      elapse_time: `${elapse_time}ms`,
     });
 
     return next.handle().pipe(
@@ -19,11 +22,12 @@ export class LoggingInterceptor implements NestInterceptor {
         console.log('Response:', {
           statusCode: response.statusCode,
           data,
+          elapse_time: `${elapse_time}ms`,
         });
       }),
 
       catchError((error) => {
-        console.error('Error:', error);
+        console.error('Error:', error, `After ${elapse_time}ms`);
         throw error;
       })
     );
